29f4ff2ff6aa69b39e28c8f3b86fb86d944195ed
Jacoub-Bakir <88115043+Jacoub-Bakir@users.noreply.github.com>
2023-01-29 22:06:58 +0100

    Add files via upload

---
+++ TemperaturHumiditySensorCoontroller/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturHumiditySensorCoontroller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel ch = new TcpChannel(8081);
+            ChannelServices.RegisterChannel(ch,false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Temperature_Humidity_Imp), "Temperature_Humidity", WellKnownObjectMode.SingleCall);
+            Console.WriteLine("Temperatur_Humidity Sensor Coontroller Server is Ready");
+            Console.Read();
+        }
+    }
+}
---
+++ TemperaturHumiditySensorCoontroller/Temperature_Humidity_Imp.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemperaturHumiditySensorCoontroller
+{
+    class Temperature_Humidity_Imp : MarshalByRefObject, RemoteInerfaces.Temperatur_HumidityInterface, RemoteInerfaces.Services.IDeviceService
+    {
+        private bool isDeviceActive = true;
+
+        public void changeDeviceState(bool isActive)
+        {
+            isDeviceActive = isActive;
+            if (isActive)
+            {
+                Console.WriteLine("Device is Active");
+            }
+            else
+            {
+                Console.WriteLine("Device is Desactive");
+            }
+        }
+
+        public string getHumidity()
+        {
+            if (isDeviceActive)
+            {
+                Random rnd = new Random();
+                int hum = rnd.Next(0, 100);
+                return hum.ToString() + ".0 %";
+            }
+            return "";
+        }
+
+        public string getTemperature()
+        {
+            if (isDeviceActive)
+            {
+                Random rnd = new Random();
+                int tem = rnd.Next(-50, 100);
+                return tem.ToString() + ".0 °C";
+            }
+            return "";
+        }
+    }
+}
+
+
